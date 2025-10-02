b918556a6f1aa633a3d38d4a0bf9030ad276b083
Fuzail <fuzailahmad672@gmail.com>
2025-10-02 23:39:09 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "Project-SG" {
+  name        = "Project-SG"
+  description = "Open 22, 443, 80, 8080, 9000"
+  ingress = [
+    for port in [22, 443, 80, 8080, 9000, 3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Project-SG"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-02d26659fd82cf299"
+  instance_type          = "t2.large"
+  key_name               = "Fuzail-key"
+  vpc_security_group_ids = [aws_security_group.Project-SG.id]
+  user_data              = templatefile("./resource.sh", {})
+
+  tags = {
+    Name = "Swiggy Project"
+  }
+  root_block_device {
+    volume_size = 25
+  }
+}
